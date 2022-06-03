a1751018e126a4936b8d25673d56ef9464e4aa35
Lordzyblaze <melawzy421@yahoo.com>
2022-06-03 22:14:06 +0000

    1-last_digit.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,50 +1,27 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
 /**
- * Main - Prints a random number and states whether
- * This program will assign a random number to the variable n
+ * main - Prints a random number and states whether
  *
- * It is positive, negative, or zero.
- *
- *
+ * it is positive, negative, or zero.
  *
  * Return: Always 0.
- *
  */
-
 int main(void)
-
 {
-
 	int n;
-
-
-
 	srand(time(0));
- /* your code goes there */
 	n = rand() - RAND_MAX / 2;
-
-
-
+	/* your code goes there */
 	if (n > 0)
-
 		printf("%d is positive\n", n);
-
 	else if (n < 0)
-
 		printf("%d is negative\n", n);
-
 	else
-
 		printf("%d is zero\n", n);
 
-
-
 	return (0);
-
 }
 

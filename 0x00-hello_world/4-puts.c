32bc9133df5ffc44af2154a7dc511bad332ee721
Lordzyblaze <melawzy421@yahoo.com>
2022-06-03 02:33:41 +0000

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: this is a new program and it is not using printf.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle" );
+return (0);
+}

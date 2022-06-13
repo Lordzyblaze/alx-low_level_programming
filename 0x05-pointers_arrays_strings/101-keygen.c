4ce71ddc4a5065695318a9ba97108cb449c51e1a
Adams Adjartey <melawzy421@yahoo.com>
2022-06-13 22:17:05 +0000

    11. Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
- * main -
- * Return:
+ * main - random password generator for 101-crackme
+ *
+ * Return: always 0
  */
 int main(void)
 {
-		return (0);
+int sum = 0, n = 0, i = 0;
+char str[100];
+rand(time(NULL));
+while (sum < SUM)
+{
+if (SUM - sum < 48)
+sum -= str[--i];
+else if (SUM - sum <= 126)
+n = SUM - sum;
+else
+n = rand() % (126 - 48) + 48;
+if (n)
+{
+str[i++] = n;
+																					sum += n;
+																					}
+n = 0;
+}
+str[i] = '\0';
+printf("%s", str);
+return (0);
 }

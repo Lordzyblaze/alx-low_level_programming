ae0f4b2dd8e53c164f6e696add384550e9214577
Adams Adjartey <melawzy421@yahoo.com>
2022-06-13 22:27:22 +0000

    11. Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,25 +9,32 @@
  */
 int main(void)
 {
-int sum = 0, n = 0, i = 0;
-char str[100];
-rand(time(NULL));
-while (sum < SUM)
+int i, j, k, s;
+char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char p[58];
+srand(time(NULL));
+while (s != 2772)
 {
-if (SUM - sum < 48)
-sum -= str[--i];
-else if (SUM - sum <= 126)
-n = SUM - sum;
-else
-n = rand() % (126 - 48) + 48;
-if (n)
+i = k = s = 0;
+while ((2772 - 122) > s)
 {
-str[i++] = n;
-																					sum += n;
-																					}
-n = 0;
+j = rand() % 62;
+p[i] = c[j];
+s += c[j];
+i++;
 }
-str[i] = '\0';
-printf("%s", str);
+while (c[k])
+{
+if (c[k] == (2772 - s))
+{
+p[i] = c[k];
+s += c[k];
+i++;
+break;
+}
+k++;
+}
+p[i] = '\0';
+printf("%s", p);
 return (0);
 }

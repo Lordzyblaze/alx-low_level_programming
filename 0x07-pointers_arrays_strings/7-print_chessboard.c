b9c89b9074dc7c338fd63567ba669677496df6a1
Adams Adjartey <melawzy421@yahoo.com>
2022-06-20 22:52:37 +0000

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,6 +9,7 @@
 void print_chessboard(char (*a)[8])
 {
 int b, c;
+
 for (b = 0; b < 8; b++)
 {
 for (c = 0; c < 8; c++)
@@ -16,5 +17,5 @@ for (c = 0; c < 8; c++)
 _putchar(a[b][c]);
 }
 _putchar('\n');
-											}
+}
 }

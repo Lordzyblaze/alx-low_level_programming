7673437a91a5301f27f1d74e0cddde3528a93920
Adams Adjartey <melawzy421@yahoo.com>
2022-06-25 01:42:41 +0000

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,14 +10,14 @@
 
 int main(int argc, char *argv[])
 {
-int x;
+int i;
 
 if (argc == 1)
 printf("%d\n", argc - 1);
 else
 {
-for (x = 0; *argv; x++, argv++);
-printf("%d\n", x - 1);
+for (i = 0; *argv; i++, argv++);
+printf("%d\n", i - 1);
 }
 return (0);
 }

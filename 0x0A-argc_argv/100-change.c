e8080f6cc4769c5d2704f7146482d8f03a9844e1
Adams Adjartey <melawzy421@yahoo.com>
2022-06-26 00:19:25 +0000

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,66 +1,31 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
 
 /**
- * coinConverter - Helper function that does all the mathematics
- * @i: Passed in variable from main for calculations
- * Return: The number of coins needed minimum for the passed in variable
- */
-int coinConverter(int i)
-{
-int count = 0;
-
-while (i != 0)
-{
-if (i % 10 == 9 || i % 10 == 7)
-i -= 2;
-else if (i % 25 == 0)
-i -= 25;
-else if (i % 10 == 0)
-i -= 10;
-else if (i % 5 == 0)
-i -= 5;
-else if (i % 2 == 0)
-{
-if (i % 10 == 6)
-i -= 1;
-else
-i -= 2;
-}
-else
-i -= 1;
-count++;
-}
-return (count);
-}
-
-/**
- * main - Takes in exactly one argument for minimum coin count
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
+ * main - Prints the addition of positive numbers,
+ *        followed by a new line.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If one of the numbers contains symbols that are non-digits - 1.
+ *         Otherwise - 0.
  */
 int main(int argc, char *argv[])
 {
-int i, coin;
+int num, digit, sum = 0;
 
-coin = 0;
-
-if (argc != 2)
+for (num = 1; num < argc; num++)
+{
+for (digit = 0; argv[num][digit]; digit++)
+{
+if (argv[num][digit] < '0' || argv[num][digit] > '9')
 {
 printf("Error\n");
 return (1);
 }
-i = atoi(argv[1]);
-if (i < 0)
-printf("0\n");
-else
-{
-coin = coinConverter(i);
-printf("%d\n", coin);
 }
+sum += atoi(argv[num]);
+}
+printf("%d\n", sum);
 return (0);
 }

00fd5c342c89f0c63c6184c132851197e0f33949
Adams Adjartey <melawzy421@yahoo.com>
2022-07-18 22:31:46 +0000

    0. Print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/*
+ * File: lists.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x12-more_singly_linked_lists directory.
+ */
+
 #include <stdlib.h>
 
 /**
@@ -9,13 +16,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ * for Holberton project
  */
-
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -34,4 +40,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif 
+#endif /* LISTS_H */

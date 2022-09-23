6e90bc6b0f37e7009c3544020b7db4036743cf9e
Adams Adjartey <melawzy421@yahoo.com>
2022-09-23 16:43:40 +0000

    6. >>> del ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -20,7 +20,7 @@ void hash_table_delete(hash_table_t *ht)
 				tmp = node->next;
 				free(node->key);
 				free(node->value);
-                		free(node);
+				free(node);
 				node = tmp;
 																		}
 		}
